7fe658567ee10a1fad7413e71235df7f5639bc0e
Frédéric VIRICEL <99799464+xxxPOUPOUxxx@users.noreply.github.com>
2025-10-19 23:01:42 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/chainmonitor/main.go
@@ -0,0 +1,23 @@
+// cmd/chainmonitor/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "chainmonitor/internal/chainmonitor"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := chainmonitor.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
